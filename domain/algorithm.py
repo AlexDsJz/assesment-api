4841e93c2e2e44177d5085e9dbddc1ae511fd3bb
AlexDsJz <cduenasj1700@alumno.ipn.mx>
2022-11-08 15:35:13 -0600

    Modified

--- domain/algorithm.py
+++ domain/algorithm.py
@@ -2,7 +2,7 @@ integer_roman = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), 
 
 def decimal_to_roman(number):
     roman = ''
-    if number > 0 and number < 3999:
+    if number > 0 and number <= 3999:
         for i, r in integer_roman:
             while number >= i:
                 roman += r
